fb1b057aa554c896daf25391c914294edb4659c5
John Fink <john.fink@gmail.com>
2010-12-16 10:18:41 -0500

    Added Freemind mindmap.

---
+++ Version Control for Libraries.mm
@@ -0,0 +1,18 @@
+<map version="0.8.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1292512595887" ID="Freemind_Link_1388242621" MODIFIED="1292512603371" TEXT="Version Control for Libraries">
+<node CREATED="1292512608842" ID="_" MODIFIED="1292512610241" POSITION="right" TEXT="history">
+<node CREATED="1292512610691" ID="Freemind_Link_240177538" MODIFIED="1292512612195" TEXT="gaming">
+<node CREATED="1292512637008" ID="Freemind_Link_1061447740" MODIFIED="1292512669171" TEXT="save games">
+<node CREATED="1292512624889" ID="Freemind_Link_1238946435" MODIFIED="1292512631520" TEXT="images">
+<node CREATED="1292512690543" ID="Freemind_Link_823029795" MODIFIED="1292512697856" TEXT="save game screen for River City Rumble"/>
+</node>
+</node>
+<node CREATED="1292512685657" ID="Freemind_Link_1495944081" MODIFIED="1292512686822" TEXT="images">
+<node CREATED="1292512632084" ID="Freemind_Link_232517404" MODIFIED="1292512634026" TEXT="Super Mario"/>
+</node>
+</node>
+<node CREATED="1292512622012" ID="Freemind_Link_1430359119" MODIFIED="1292512623937" TEXT="old VC"/>
+</node>
+</node>
+</map>
